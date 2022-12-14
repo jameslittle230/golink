b01b4ac62159edd6c787c7582cc094c4208654bf
James Little <littleguy23@gmail.com>
2022-12-13 20:20:23 -0500

    Write first test and get it to pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use itertools::Itertools;
+use thiserror::Error;
+use url::{ParseError, Url};
+
+#[derive(Debug)]
+struct ExpandEnvironment {
+    remainder: Vec<String>,
+}
+
+fn expand(input: &str, environment: ExpandEnvironment) -> String {
+    dbg!(environment);
+    input.to_string()
+}
+
+#[derive(Error, Debug, Clone, Copy, PartialEq, Eq)]
+pub enum GolinkError {
+    #[error("Input unable to be parsed as URL")]
+    UrlParseError(#[from] ParseError),
+
+    #[error("No first path segment")]
+    NoFirstPathSegment,
+}
+
+pub fn resolve(
+    input: &str,
+    lookup: &dyn Fn(&str) -> Option<String>,
+) -> Result<String, GolinkError> {
+    let url = Url::parse(input)?;
+    let mut segments = url.path_segments().unwrap();
+    let short = segments.next().ok_or(GolinkError::NoFirstPathSegment)?;
+    let remainder = segments.map(|s| s.to_owned()).collect_vec();
+
+    let lookup_value = lookup(short);
+
+    Ok(expand(
+        &lookup_value.unwrap(),
+        ExpandEnvironment { remainder },
+    ))
 }
 
 #[cfg(test)]
@@ -8,7 +44,14 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let lookup = |input: &str| {
+            if input == "test" {
+                return Some("http://example.com".to_string());
+            }
+            None
+        };
+
+        let computed = resolve("http://go/test", &lookup);
+        assert_eq!(computed, Ok("http://example.com".to_string()))
     }
 }
